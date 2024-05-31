afeddd16225262246bddbf43f554ca2ce9b0cee6
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2024-05-31 12:16:22 +0200

    migrate and seed

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Type;
 
 class TypeSeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = ['Fullstack', 'Frontend', 'Backend'];
+
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->slug = Str::slug($newType->name, '-');
+            $newType->save();
+        }
     }
 }
