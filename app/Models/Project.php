4e92005c2362767ed42a754d2ebb71469c13aa37
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2024-05-31 12:23:34 +0200

    aggiunte relazioni tra project e type

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Project extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'cover_image', 'content', 'slug'];
+
+
+    /**
+     * Get the type that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
 
