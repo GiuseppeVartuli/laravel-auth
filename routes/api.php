dc47e122d3aa21370a393d9483ad716ecbf2ad5a
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2024-06-05 10:57:30 +0200

    fix bug

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\ProjectController;
+
+use App\Http\Controllers\API\Projectcontroller;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,7 +21,7 @@ use Illuminate\Support\Facades\Route;
     return $request->user();
 });*/
 
-Route::get('projects',[ProjectController::class, 'index'] );
+Route::get('projects',[Projectcontroller::class, 'index'] );
 
-Route::get('projects/{project}',[ProjectController::class, 'show'] );
+Route::get('projects/{project}',[Projectcontroller::class, 'show'] );
 
